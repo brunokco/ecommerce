a0c83142866b1cf1f665865f6e9a0ed31f828ef8
bruno bruzzone <brunoelkpo19@gmail.com>
2020-08-28 17:11:03 -0300

    Cambios Minimos

--- js/products.js
+++ js/products.js
@@ -85,6 +85,8 @@ function sortAndShowCategories(sortCriteria, categoriesArray){
     showCategoriesList();
 }
 
+
+
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
@@ -139,4 +141,4 @@ document.addEventListener("DOMContentLoaded", function(e){
 
         showCategoriesList();
     });
-});
+})
